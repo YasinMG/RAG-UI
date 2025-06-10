fae4f92be5d827ca366fcbe2880f519257af6bdd
Yasein_Shikhada <104557936+YasinMG@users.noreply.github.com>
2025-06-10 08:54:28 +0200

    Delete startup.sh

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pip install -r requirements.txt
-
-# Optional: Logging anzeigen
-echo "Starting Streamlit..."
-
-# Starte Streamlit auf dem von Azure freigegebenen Port ($PORT)
-streamlit run main.py --server.port $PORT --server.address 0.0.0.0
